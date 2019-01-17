9d1555e0f2bc493f725c8f8eefc21e5c00fd3659
Felipe Mantovani <felipexmantovani@gmail.com>
2019-01-17 12:40:52 -0200

    Criado controller das tarefas

---
+++ api/controllers/tarefas.js
@@ -0,0 +1,58 @@
+import HttpStatus from 'http-status';
+
+const defaultResponse = (data, statusCode = HttpStatus.OK) => ({
+    data: data,
+    statusCode: statusCode
+});
+
+const errorResponse = (message, statusCode = HttpStatus.BAD_REQUEST) =>
+    defaultResponse(
+        {
+            error: message
+        },
+        statusCode
+    );
+
+class TarefasController {
+    constructor(Tarefas) {
+        this.Tarefas = Tarefas;
+    }
+
+    getAll() {
+        return this.Tarefas.findAll({})
+            .then(result => defaultResponse(result))
+            .catch(error => errorResponse(error.message));
+    }
+
+    getById(params) {
+        return this.Tarefas.findOne({
+            where: params
+        })
+            .then(result => defaultResponse(result))
+            .catch(error => errorResponse(error.message));
+    }
+
+    create(data) {
+        return this.Tarefas.create(data)
+            .then(result => defaultResponse(result, HttpStatus.CREATED))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY));
+    }
+
+    update(data, params) {
+        return this.Tarefas.update(data, {
+            where: params
+        })
+            .then(result => defaultResponse(result))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY));
+    }
+
+    delete(params) {
+        return this.Tarefas.destroy({
+            where: params
+        })
+            .then(result => defaultResponse(result, HttpStatus.NO_CONTENT))
+            .catch(error => errorResponse(error.message, HttpStatus.UNPROCESSABLE_ENTITY));
+    }
+}
+
+export default TarefasController;
