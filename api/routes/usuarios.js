cc8096e5496e6c56dcb92e4a38ed69fe59617099
Felipe Mantovani <felipexmantovani@gmail.com>
2019-01-16 22:58:19 -0200

    Adicionado mensagens de retorno no json

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -7,32 +7,86 @@ export default app => {
 
     app.route('/api/usuarios')
         .post((req, res) => {
-            usuariosController.create(req.body).then(response => {
-                res.status(response.statusCode);
-                res.json(response.data);
-            });
+            usuariosController
+                .create(req.body)
+                .then(response => {
+                    res.status(response.statusCode);
+                    res.json({
+                        msg: 'Usuário cadastrado com sucesso!',
+                        type: 'success',
+                        statusCode: response.statusCode
+                    });
+                })
+                .catch(response => {
+                    res.json({
+                        msg: 'Erro ao cadastrar o usuário!',
+                        type: 'error',
+                        statusCode: response.statusCode
+                    });
+                });
         })
         .all(app.auth.authenticate())
         .get((req, res) => {
-            usuariosController.getAll().then(response => {
-                res.status(response.statusCode);
-                res.json(response.data);
-            });
+            usuariosController
+                .getAll()
+                .then(response => {
+                    res.status(response.statusCode);
+                    res.json({
+                        usuarios: response.data,
+                        msg: 'Usuários listados com sucesso!',
+                        type: 'success',
+                        statusCode: response.statusCode
+                    });
+                })
+                .catch(response => {
+                    res.json({
+                        msg: 'Erro ao cadastrar listar usuários!',
+                        type: 'error',
+                        statusCode: response.statusCode
+                    });
+                });
         });
 
     app.route('/api/usuarios/:id')
         .all(app.auth.authenticate())
         .get((req, res) => {
-            usuariosController.getById(req.params).then(response => {
-                res.status(response.statusCode);
-                res.json(response.data);
-            });
+            usuariosController
+                .getById(req.params)
+                .then(response => {
+                    res.status(response.statusCode);
+                    res.json({
+                        usuario: response.data,
+                        msg: 'Cadastro de usuário encontrado!',
+                        type: 'success',
+                        statusCode: response.statusCode
+                    });
+                })
+                .catch(response => {
+                    res.json({
+                        msg: 'Cadastro de usuário não encontrado!',
+                        type: 'error',
+                        statusCode: response.statusCode
+                    });
+                });
         })
         .put((req, res) => {
-            usuariosController.update(req.body, req.params).then(response => {
-                res.status(response.statusCode);
-                res.json(response.data);
-            });
+            usuariosController
+                .update(req.body, req.params)
+                .then(response => {
+                    res.status(response.statusCode);
+                    res.json({
+                        msg: 'Usuário atualizado!',
+                        type: 'success',
+                        statusCode: response.statusCode
+                    });
+                })
+                .catch(response => {
+                    res.json({
+                        msg: 'Não foi possível atualizar o usuário!',
+                        type: 'error',
+                        statusCode: response.statusCode
+                    });
+                });
         })
         .delete((req, res) => {
             usuariosController.delete(req.params).then(response => {
